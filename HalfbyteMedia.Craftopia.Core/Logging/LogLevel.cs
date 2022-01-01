383b8598f3cd445c8305b3a08d4eb188a3e984f8
Jason Harris <jasonharris306@gmail.com>
2022-01-01 18:43:53 +0000

    Added Core Lib

---
+++ HalfbyteMedia.Craftopia.Core/Logging/LogLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalfbyteMedia.Craftopia.Core.Logging
+{
+    public enum LogLevel
+    {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        VERBOSE,
+    }
+}
---
+++ HalfbyteMedia.Craftopia.Core/Logging/Logger.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalfbyteMedia.Craftopia.Core.Logging
+{
+    public class Logger
+    {
+        public string LogDirectory { get; }
+        public string LogName { get; }
+        public string LogPath { get; private set; }
+
+        public Logger(string logDirectory, string logName = "log.txt")
+        {
+            LogDirectory = logDirectory;
+            LogName = logName;
+
+            LogPath = Path.Combine(logDirectory, logName);
+
+            InitializeLogger();
+        }
+
+
+        private void InitializeLogger()
+        {
+            try
+            {
+                if (!Directory.Exists(LogDirectory))
+                    Directory.CreateDirectory(LogDirectory);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private string GetLogLine(string text, LogLevel logLevel)
+        {            
+            return $"[{DateTime.Now:G}][{logLevel}]: {text}{Environment.NewLine}";
+        }
+
+        public void WriteLine(string text, LogLevel logLevel)
+        {
+            var logLine = GetLogLine(text, logLevel);
+
+            if (logLevel == LogLevel.ERROR) Console.Error.Write(logLine);
+            else Console.Write(logLine);
+
+            try
+            {
+                File.AppendAllText(LogPath, logLine);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void WriteInfo(string text)
+        {
+            WriteLine(text, LogLevel.INFO);
+        }
+
+        public void WriteDebug(string text)
+        {
+            WriteLine(text, LogLevel.DEBUG);
+        }
+
+        public void WriteWarn(string text)
+        {
+            WriteLine(text, LogLevel.WARN);
+        }
+
+        public void WriteError(string text)
+        {
+            WriteLine(text, LogLevel.ERROR);
+        }
+
+        public void WriteVerbose(string text)
+        {
+            WriteLine(text, LogLevel.VERBOSE);
+        }
+    }
+}
