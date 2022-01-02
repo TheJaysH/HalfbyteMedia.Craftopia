df9016457158a6f9ae12c549a233354b92207722
Jason Harris <jasonharris306@gmail.com>
2022-01-02 02:46:10 +0000

    Core: Modify Web Utils

---
+++ HalfbyteMedia.Craftopia.Core/Utilities/WebUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalfbyteMedia.Craftopia.Core.Utilities
+{
+    public class WebUtils
+    {
+        public static void DownloadFile(string sourceUrl, string destPath)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(new Uri(sourceUrl), destPath);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+           
+        }
+
+        public static void DownloadFileAsync(string sourceUrl, string destPath)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFileAsync(new Uri(sourceUrl), destPath);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
