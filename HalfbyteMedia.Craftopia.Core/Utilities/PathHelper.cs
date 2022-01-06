6501508e108be6e1f7b8a0797818618869f97cdc
Jason Harris <jasonharris306@gmail.com>
2022-01-06 13:23:28 +0000

    Core: Added misc utilities

---
+++ HalfbyteMedia.Craftopia.Core/Utilities/PathHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalfbyteMedia.Craftopia.Core.Utilities
+{
+    public static class PathHelper
+    {
+        public static bool IsValidPath(this DirectoryInfo directoryInfo)
+        {
+            try
+            {
+                if (!directoryInfo.Exists)
+                {
+                    directoryInfo.Create();
+                    directoryInfo.Delete();
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
